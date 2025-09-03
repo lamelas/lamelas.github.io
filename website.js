2f1c4bab24edc5a768934ea4f528e220d1f0aa2a
André Lamelas <andre@lamelas.org>
2025-09-03 15:35:42 +0100

    More fixes to the design

--- website.js
+++ website.js
@@ -1,23 +1,48 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const h2Elements = document.querySelectorAll('h2');
-    h2Elements.forEach(h2 => {
-        h2.classList.add('closed');
-        const panel = h2.nextElementSibling;
-        if (!panel) return;
-        panel.classList.add('collapsible', 'collapsed');
-        panel.style.maxHeight = '0px';
+"use strict";
 
-        h2.onclick = function() {
-            const content = this.nextElementSibling;
-            if (!content) return;
-            content.classList.toggle('collapsed');
-            this.classList.toggle('closed');
+document.addEventListener("DOMContentLoaded", function () {
+  // Fun idea to hide my email address from spammers
+  // From here: https://spencermortensen.com/articles/email-obfuscation/#link-entity
+  const a = document.getElementById("link-conversion");
+  a.setAttribute(
+    "href",
+    a
+      .getAttribute("href")
+      .replace("-", ":")
+      .replace("-", "@")
+      .replace("send", "mailto")
+      .replace("me", "andre")
+      .replace("an-email/", "lamelas.org")
+  );
 
-            if (content.classList.contains('collapsed')) {
-                content.style.maxHeight = '0px';
-            } else {
-                content.style.maxHeight = content.scrollHeight + 'px';
-            }
-        }
-    });
+  // Accordion menus
+  const baseScroll = 150;
+  const defaultTransitionDuration = getComputedStyle(document.documentElement).getPropertyValue("--transition-duration").trim().replace("ms", "");
+
+  const h2Elements = document.querySelectorAll("h2");
+  h2Elements.forEach((h2) => {
+    h2.classList.add("closed");
+    const panel = h2.nextElementSibling;
+    if (!panel) return;
+    panel.classList.add("collapsible", "collapsed");
+    panel.style.maxHeight = "0px";
+
+    h2.onclick = function () {
+      const content = this.nextElementSibling;
+      if (!content) return;
+
+      const transitionDuration = content.scrollHeight/baseScroll * defaultTransitionDuration;
+      const speed = transitionDuration > 3000 ? 3000 : transitionDuration;
+      
+      content.classList.toggle("collapsed");
+      this.classList.toggle("closed");
+
+      if (content.classList.contains("collapsed")) {
+        content.style.maxHeight = "0px";
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+        content.style.transitionDuration = speed + "ms";
+      }
+    };
+  });
 });
